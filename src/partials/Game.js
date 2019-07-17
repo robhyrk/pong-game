311f44c5e92d19e64cec0817c7ab435e62f9b8c8
robhyrkiel <rob.hyrkiel@gmail.com>
2019-07-17 10:10:10 -0400

    feat:added in svg canvas

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,26 @@
+import {SVG_NS} from '../settings'
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
-		// Other code goes here...
+		this.gameElement = document.getElementById(this.element)
   }
 
   render() {
-		// More code goes here....
+    
+    //Clear board
+     this.gameElement.innerHTML = ''
+
+    //create SVG element for the board
+    let svg = document.createElementNS(SVG_NS, 'svg')
+
+    svg.setAttributeNS(null, 'width', this.width)
+    svg.setAttributeNS(null, 'height', this.height)
+    svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`)
+    this.gameElement.appendChild(svg)
+
   }
 }
